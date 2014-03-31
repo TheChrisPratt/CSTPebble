136a419b564746c862a00b5d5cfe24f94779a19a
Chris Pratt <chris@anodyzed.com>
2014-03-30 23:49:52 -0700

    Beginnings of Settings Support

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,16 @@
+// From https://github.com/BalestraPatrick/PebbleConfigurationExample
+
+Pebble.addEventListener("ready",function(event) {
+  console.log("Ready");
+});
+
+Pebble.addEventListener("showConfiguration",function(event) {
+  Pebble.openURL("http://www.planetpratt.com/cstpebble/settings.html");
+});
+
+Pebble.addEventListener("webviewclosed",function(event) {
+  console.log(event.response);
+  var responseFromWebView = decodeURIComponent(event.response);
+  var settings = JSON.parse(responseFromWebView);
+  Pebble.sendAppMessage(settings);
+});
